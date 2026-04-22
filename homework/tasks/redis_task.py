ea7619731cfa5912ebcea00c3edbc932fcafa5b3
tsvetcov <37505603+tsvetcov@users.noreply.github.com>
2026-04-22 04:07:09 +0300

    Update redis_task.py

--- homework/tasks/redis_task.py
+++ homework/tasks/redis_task.py
@@ -17,11 +17,12 @@ class UsersByTitleStorage:
         можно было за один запрос получить список уникальных пользователей,
         имеющих объявления с заданным заголовком.
         """
-        # YOUR CODE GOES HERE
+        aredis.sadd(title, user_id)
 
     async def find_users_by_title(self, title: str) -> list[int]:
         """
         Напишите код для поиска уникальных user_id, имеющих хотя бы одно объявление
         с заданным title.
         """
-        # YOUR CODE GOES HERE
+        result = aredis.smembers(title)
+        return list(result)
